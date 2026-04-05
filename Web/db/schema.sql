e1cf47f37bb58ef346a3e6c7eb03d295696d5f71
LeonardoGarcia29 <155427987+LeonardoGarcia29@users.noreply.github.com>
2026-04-05 16:04:13 -0700

    Update Deletes table to include DeletedByUserID

--- Web/db/schema.sql
+++ Web/db/schema.sql
@@ -61,12 +61,12 @@ CREATE TABLE Disables (
 );
 
 CREATE TABLE Deletes (
-    AdminUserID INTEGER,
+    DeletedByUserID INTEGER,
     SessionID INTEGER,
     Reason VARCHAR(200),
     PRIMARY KEY (AdminUserID, SessionID),
     UNIQUE (SessionID),
-    FOREIGN KEY (AdminUserID) REFERENCES Users(UserID),
+    FOREIGN KEY (DeletedByUserID) REFERENCES Users(UserID),
     FOREIGN KEY (SessionID) REFERENCES StudySession(SessionID)
 );
 
